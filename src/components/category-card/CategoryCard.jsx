df68e5c5735a6f932dee687a2ad81ba2b967f1c6
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-22 12:41:58 +0530

    refactor: added images in categories

--- src/components/category-card/CategoryCard.jsx
+++ src/components/category-card/CategoryCard.jsx
@@ -1,13 +1,13 @@
 import "./category-card.css";
 import { Link } from "react-router-dom";
 
-const CategoryCard = ({ categoryData: { categoryName } }) => {
+const CategoryCard = ({ categoryData: { categoryName, image } }) => {
   return (
     <Link className="cursor card-pos-rel" to={`/category/${categoryName}`}>
       <div className="category-image-container">
         <img
           className="image-resp border-radius"
-          src="https://scontent.fhyd11-2.fna.fbcdn.net/v/t39.30808-6/225264609_10159440989214146_3456298355581072175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=109&ccb=1-5&_nc_sid=dd9801&_nc_ohc=JyKx1UcAYR8AX-8UqNO&_nc_ht=scontent.fhyd11-2.fna&oh=00_AT9zAKU5KmMI71eEIkvJWqr2xPgkxQRs_kh7uiX3wWscvg&oe=623C667A"
+          src={image}
           alt={categoryName}
         />
       </div>
