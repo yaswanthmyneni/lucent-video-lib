8266f0c0624657be920c469a2d088528c167d044
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-23 11:11:10 +0530

    refactor: removed fragment

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -14,28 +14,23 @@ const HomePage = () => {
   }, []);
 
   return (
-    <>
-      <main className="home">
-        <section className="hero">
-          <img
-            className="image-resp"
-            src="https://scontent.fhyd11-2.fna.fbcdn.net/v/t39.30808-6/225264609_10159440989214146_3456298355581072175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=109&ccb=1-5&_nc_sid=dd9801&_nc_ohc=JyKx1UcAYR8AX-8UqNO&_nc_ht=scontent.fhyd11-2.fna&oh=00_AT9zAKU5KmMI71eEIkvJWqr2xPgkxQRs_kh7uiX3wWscvg&oe=623C667A"
-            alt="hero"
-          />
-        </section>
-        <section className="category">
-          <h2>Categories:</h2>
-          <div className="category-container flex gap-1rem">
-            {categoryList.map((categoryData) => (
-              <CategoryCard
-                key={categoryData._id}
-                categoryData={categoryData}
-              />
-            ))}
-          </div>
-        </section>
-      </main>
-    </>
+    <main className="home">
+      <section className="hero">
+        <img
+          className="image-resp"
+          src="https://scontent.fhyd11-2.fna.fbcdn.net/v/t39.30808-6/225264609_10159440989214146_3456298355581072175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=109&ccb=1-5&_nc_sid=dd9801&_nc_ohc=JyKx1UcAYR8AX-8UqNO&_nc_ht=scontent.fhyd11-2.fna&oh=00_AT9zAKU5KmMI71eEIkvJWqr2xPgkxQRs_kh7uiX3wWscvg&oe=623C667A"
+          alt="hero"
+        />
+      </section>
+      <section className="category">
+        <h2>Categories:</h2>
+        <div className="category-container flex gap-1rem">
+          {categoryList.map((categoryData) => (
+            <CategoryCard key={categoryData._id} categoryData={categoryData} />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 };
 
