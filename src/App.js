f9518e762012f2709ed4e92bd5343ea453127689
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-24 16:31:48 +0530

    feat: added category paths and used useLocation hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./utility.css";
-import { Routes, Route } from "react-router-dom";
-import { HomePage, MockAPI } from "pages";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { HomePage, MockAPI, VideoListingPage } from "pages";
 import { Footer, Navigation } from "components";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/category">
+          <Route path="youth-and-truth" element={<VideoListingPage />} />
+          <Route path="yoga-sessions" element={<VideoListingPage />} />
+          <Route path="yogic-lifestyle" element={<VideoListingPage />} />
+        </Route>
         <Route path="/mockman" element={<MockAPI />} />
       </Routes>
-      <Footer />
+
+      {location.pathname === "/" && <Footer />}
     </>
   );
 }
