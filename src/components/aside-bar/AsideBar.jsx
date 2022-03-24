b1767967e2d1298efbe5e07aa8ffa879196d732c
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-24 16:23:36 +0530

    feat: added aside bar

---
+++ src/components/aside-bar/AsideBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import "./aside-bar.css";
+
+const AsideBar = () => {
+  return (
+    <aside className="aside">
+      <div className="aside-div">
+        <Link to="/watchlater">
+          <i className="fa-solid fa-clock aside-fa cursor"></i>
+        </Link>
+        <Link to="/playlist">
+          <i className="fa-solid fa-list aside-fa cursor"></i>
+        </Link>
+        <Link to="/history">
+          <i className="fa-solid fa-clock-rotate-left aside-fa cursor"></i>
+        </Link>
+      </div>
+    </aside>
+  );
+};
+
+export { AsideBar };
