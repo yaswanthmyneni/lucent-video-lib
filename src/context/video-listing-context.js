26970a528399ec2eb85f179884bda587c1af9f2d
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-24 16:28:07 +0530

    feat: added video listing page context

---
+++ src/context/video-listing-context.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const VideoListing = createContext();
+const useVideoListingContext = () => useContext(VideoListing);
+
+const VideoListingProvider = ({ children }) => {
+  const [videoList, setVideoList] = useState([]);
+
+  // getting videos data
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get("/api/videos");
+        if (response.status === 200) {
+          setVideoList(response.data.videos);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  const value = { videoList };
+
+  return <VideoListing.Provider value={value}>{children}</VideoListing.Provider>;
+};
+
+export { useVideoListingContext, VideoListingProvider };
