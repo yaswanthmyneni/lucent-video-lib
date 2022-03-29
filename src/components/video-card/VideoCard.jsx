86c49e805dc370209268754a470fb835b05e1b6c
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-29 19:06:49 +0530

    style: changed style of video card

--- src/components/video-card/VideoCard.jsx
+++ src/components/video-card/VideoCard.jsx
@@ -1,20 +1,46 @@
+import {
+  BsThreeDotsVertical,
+  MdWatchLater,
+  MdDelete,
+} from "assets/icons/icons";
 import "./video-card.css";
 
-const VideoCard = ({ videoData: { title, image }, watchLater }) => {
+const VideoCard = ({
+  cardData: { title, image, watchLater, history },
+  btnNameOne,
+  addToHistory,
+  removeFromHistory,
+  addToWatchLater,
+  removeFromWatchLater,
+}) => {
   return (
     <>
       <div className="video-card-container card-pos-rel">
-        <i className="fa-solid fa-heart cursor"></i>
+        {history ? (
+          <MdDelete
+            className="video-card-icon cursor color-red"
+            onClick={removeFromHistory}
+          />
+        ) : (
+          <MdWatchLater
+            className={`video-card-icon cursor ${
+              watchLater ? "color-green" : "color-black"
+            }`}
+            onClick={watchLater ? removeFromWatchLater : addToWatchLater}
+          />
+        )}
         <div className="video-card-image-container">
           <img className="image-resp" src={image} alt={title} />
         </div>
         <div className="card-margin">
-          <h5 className="card-margin-0">{title}</h5>
+          <div className="flex video-card-flex-adjustment">
+            <h5>{title}</h5>
+            <BsThreeDotsVertical className="cursor" />
+          </div>
           <small className="text-gray">6k views | 4 hours ago</small>
         </div>
-        <button className="btn btn-primary m-b-4px">Watch Now</button>
-        <button className={watchLater ? `btn btn-danger` : `btn btn-primary`}>
-          {watchLater ? "Remove from watch later" : "Watch later"}
+        <button className="btn btn-primary m-b-4px" onClick={addToHistory}>
+          {btnNameOne}
         </button>
       </div>
     </>
