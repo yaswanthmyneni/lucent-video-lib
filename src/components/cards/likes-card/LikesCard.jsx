0e02ff27fa1298f55a9a40a7ce5b40cdca9d81f8
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-06-02 11:17:02 +0530

    style: like icon position changed

--- src/components/cards/likes-card/LikesCard.jsx
+++ src/components/cards/likes-card/LikesCard.jsx
@@ -3,29 +3,32 @@ import { useNavigate } from "react-router-dom";
 
 const LikesCard = ({ cardData, btnNameOne, removeFromLikedVideos }) => {
   const { _id, title, image } = cardData;
+
+  // from react-router-dom
   const navigate = useNavigate();
 
   return (
-    <>
-      <div className="video-card-container card-pos-rel">
-        <AiFillLike
-          className="video-card-icon cursor color-green"
-          onClick={removeFromLikedVideos}
-        />
-        <div className="video-card-image-container">
-          <img className="image-resp" src={image} alt={title} />
-        </div>
-        <div className="card-margin">
-          <div className="flex video-card-flex-adjustment">
-            <h5>{title}</h5>
-          </div>
-          <small className="text-gray">6k views | 4 hours ago</small>
-        </div>
-        <button className="btn btn-primary" onClick={() => navigate(`/video/${_id}`)}>
-          {btnNameOne}
-        </button>
+    <section className="video-card-container card-pos-rel">
+      <div className="video-card-image-container">
+        <img className="image-resp" src={image} alt={title} />
       </div>
-    </>
+      <div className="card-margin">
+        <div className="flex video-card-flex-adjustment">
+          <h5>{title}</h5>
+          <AiFillLike
+            className="video-card-icon cursor color-green"
+            onClick={removeFromLikedVideos}
+          />
+        </div>
+        <small className="text-gray">6k views | 4 hours ago</small>
+      </div>
+      <button
+        className="btn btn-primary"
+        onClick={() => navigate(`/video/${_id}`)}
+      >
+        {btnNameOne}
+      </button>
+    </section>
   );
 };
 
--- src/components/cards/video-card/VideoCard.jsx
+++ src/components/cards/video-card/VideoCard.jsx
@@ -30,25 +30,28 @@ const VideoCard = ({
     setDropdown(!dropdown);
   };
 
+  // from react-router-dom
   const navigate = useNavigate();
   const location = useLocation();
 
+  // from localStorage
   const encodedToken = localStorage.getItem("token");
+
   return (
-    <>
-      <div className="video-card-container card-pos-rel">
-        <AiFillLike
-          className={`video-card-icon cursor ${
-            isLiked ? "color-green" : "color-black"
-          }`}
-          onClick={isLiked ? removeFromLikedVideos : addToLikedVideos}
-        />
-        <div className="video-card-image-container">
-          <img className="image-resp" src={image} alt={title} />
-        </div>
-        <div className="card-margin">
-          <div className="flex video-card-flex-adjustment">
-            <h5>{title}</h5>
+    <section className="video-card-container card-pos-rel">
+      <div className="video-card-image-container">
+        <img className="image-resp" src={image} alt={title} />
+      </div>
+      <div className="card-margin">
+        <div className="flex video-card-flex-adjustment">
+          <h5>{title}</h5>
+          <div className="flex align-items-center gap-8px">
+            <AiFillLike
+              className={`video-card-icon cursor ${
+                isLiked ? "color-green" : "color-black"
+              }`}
+              onClick={isLiked ? removeFromLikedVideos : addToLikedVideos}
+            />
             <BsThreeDotsVertical
               className="cursor"
               onClick={() => {
@@ -58,49 +61,49 @@ const VideoCard = ({
               }}
             />
           </div>
-          <small className="text-gray">6k views | 4 hours ago</small>
         </div>
-        <button
-          className="btn btn-primary"
-          onClick={() => navigate(`/video/${_id}`)}
-        >
-          {btnNameOne}
-        </button>
-        {dropdown && (
-          <div className="dropdown">
-            <ul className="ul-none dropdown-ul">
-              <li
-                className="dropdown-li cursor"
-                onClick={
-                  watchLater
-                    ? updatedRemoveFromWatchLater
-                    : updatedAddToWatchLater
-                }
-              >
-                <MdOutlineWatchLater />
-                {watchLater ? "Remove from watchlater" : "Add to watchlater"}
-              </li>
-              <li
-                className="dropdown-li cursor"
-                onClick={
-                  encodedToken
-                    ? () => {
-                        playlistDispatch({
-                          type: "SHOW_PLAYLIST_MODAL",
-                          payload: true,
-                        });
-                        setDropdown(!dropdown);
-                      }
-                    : () => navigate("/sign-in", { state: { from: location } })
-                }
-              >
-                <MdPlaylistAdd /> playlist
-              </li>
-            </ul>
-          </div>
-        )}
+        <small className="text-gray">6k views | 4 hours ago</small>
       </div>
-    </>
+      <button
+        className="btn btn-primary"
+        onClick={() => navigate(`/video/${_id}`)}
+      >
+        {btnNameOne}
+      </button>
+      {dropdown && (
+        <div className="dropdown">
+          <ul className="ul-none dropdown-ul">
+            <li
+              className="dropdown-li cursor"
+              onClick={
+                watchLater
+                  ? updatedRemoveFromWatchLater
+                  : updatedAddToWatchLater
+              }
+            >
+              <MdOutlineWatchLater />
+              {watchLater ? "Remove from watchlater" : "Add to watchlater"}
+            </li>
+            <li
+              className="dropdown-li cursor"
+              onClick={
+                encodedToken
+                  ? () => {
+                      playlistDispatch({
+                        type: "SHOW_PLAYLIST_MODAL",
+                        payload: true,
+                      });
+                      setDropdown(!dropdown);
+                    }
+                  : () => navigate("/sign-in", { state: { from: location } })
+              }
+            >
+              <MdPlaylistAdd /> playlist
+            </li>
+          </ul>
+        </div>
+      )}
+    </section>
   );
 };
 
